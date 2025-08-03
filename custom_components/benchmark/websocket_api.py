69744aa2315c914b5e40d0a9d7fec3f60afaa211
Jarnsen <jarno.karsch@vodafone.de>
2025-08-03 21:51:08 +0200

    Update websocket_api.py

--- custom_components/benchmark/websocket_api.py
+++ custom_components/benchmark/websocket_api.py
@@ -2,42 +2,31 @@ import os
 import json
 from homeassistant.components import websocket_api
 from .services import run_benchmark
-from .const import DATA_FILE, DOMAIN
+from .const import DOMAIN, DATA_FILE
 
 @websocket_api.websocket_command({ "type": f"{DOMAIN}/start" })
 @websocket_api.require_admin
 def ws_start(hass, connection, msg):
+    # Benchmark durchführen
     entry = hass.config_entries.async_entries(DOMAIN)[0]
-    results = run_benchmark(hass, entry)
-    history_file = hass.config.path(DATA_FILE)
-    data = json.load(open(history_file))
-    data.append(results)
-    with open(history_file, "w") as fp:
-        json.dump(data, fp)
-    connection.send_message(websocket_api.result_message(msg["id"], results))
+    result = run_benchmark(hass)
+
+    # In History speichern
+    path = hass.config.path(DATA_FILE)
+    history = json.load(open(path))
+    history.append(result)
+    with open(path, "w") as f:
+        json.dump(history, f)
+
+    # Ergebnis zurück an UI
+    connection.send_message(websocket_api.result_message(msg["id"], result))
 
 @websocket_api.websocket_command({ "type": f"{DOMAIN}/get_history" })
 def ws_history(hass, connection, msg):
-    history = json.load(open(hass.config.path(DATA_FILE)))
+    path = hass.config.path(DATA_FILE)
+    history = json.load(open(path))
     connection.send_message(websocket_api.result_message(msg["id"], history))
-
-@websocket_api.websocket_command({ "type": f"{DOMAIN}/get_settings" })
-def ws_get_settings(hass, connection, msg):
-    entry = hass.config_entries.async_entries(DOMAIN)[0]
-    connection.send_message(websocket_api.result_message(msg["id"], entry.options))
-
-@websocket_api.websocket_command({
-    "type": f"{DOMAIN}/update_settings",
-    "options": dict
-})
-@websocket_api.require_admin
-def ws_update_settings(hass, connection, msg):
-    entry = hass.config_entries.async_entries(DOMAIN)[0]
-    hass.config_entries.async_update_entry(entry, options=msg["options"])
-    connection.send_message(websocket_api.result_message(msg["id"], entry.options))
 
 def async_register(hass):
     websocket_api.async_register_command(hass, ws_start)
     websocket_api.async_register_command(hass, ws_history)
-    websocket_api.async_register_command(hass, ws_get_settings)
-    websocket_api.async_register_command(hass, ws_update_settings)
