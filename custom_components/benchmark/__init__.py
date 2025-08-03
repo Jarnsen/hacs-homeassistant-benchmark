a65fdfc20625c659b32b636f70c2c3f5536bf437
Jarnsen <jarno.karsch@vodafone.de>
2025-08-03 21:50:05 +0200

    Update __init__.py

--- custom_components/benchmark/__init__.py
+++ custom_components/benchmark/__init__.py
@@ -8,20 +8,17 @@ async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    # History-File anlegen, falls nicht existiert
-    history = hass.config.path(DATA_FILE)
-    if not os.path.exists(history):
-        with open(history, "w") as fp:
-            json.dump([], fp)
+    # History-Datei anlegen
+    path = hass.config.path(DATA_FILE)
+    if not os.path.exists(path):
+        with open(path, "w") as f:
+            json.dump([], f)
 
     # WebSocket-API registrieren
     from . import websocket_api
     websocket_api.async_register(hass)
 
-    # Sensor-Plattform laden
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     return True
