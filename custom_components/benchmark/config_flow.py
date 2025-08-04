d4e02930cb8dd8b7d73116d0dadcd9bd9148c414
Jarnsen <jarno.karsch@vodafone.de>
2025-08-04 18:30:11 +0200

    Update config_flow.py

--- custom_components/benchmark/config_flow.py
+++ custom_components/benchmark/config_flow.py
@@ -2,13 +2,8 @@ import voluptuous as vol
 from homeassistant import config_entries
 from .const import DOMAIN
 
-STEP_USER_DATA_SCHEMA = vol.Schema({})
-
 class BenchmarkConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         if user_input is None:
-            return self.async_show_form(
-                step_id="user",
-                data_schema=STEP_USER_DATA_SCHEMA
-            )
+            return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
         return self.async_create_entry(title="Benchmark", data={})
