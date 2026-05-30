408c982c8add42e3db2b3a37494d191d7d8284ac
Jarnsen <jarno.karsch@vodafone.de>
2026-05-30 23:47:04 +0200

    Add restart benchmark button

--- custom_components/benchmark/button.py
+++ custom_components/benchmark/button.py
@@ -1,17 +1,32 @@
+from __future__ import annotations
+
 from homeassistant.components.button import ButtonEntity
-from .const import DOMAIN
+
+from .const import ATTRIBUTION, DATA_DEVICE, DOMAIN
+
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    device = hass.data[f"{DOMAIN}_device"]
-    async_add_entities([BenchmarkStartButton(hass, device)], True)
+    device = hass.data[DATA_DEVICE]
+    async_add_entities(
+        [
+            BenchmarkActionButton(hass, entry.entry_id, device, "start", "Start Benchmark", "mdi:play-speed", "start"),
+            BenchmarkActionButton(hass, entry.entry_id, device, "restart", "Restart Benchmark", "mdi:restart", "restart_benchmark"),
+        ]
+    )
 
-class BenchmarkStartButton(ButtonEntity):
-    def __init__(self, hass, device_entry):
+
+class BenchmarkActionButton(ButtonEntity):
+    _attr_has_entity_name = True
+    _attr_attribution = ATTRIBUTION
+    _attr_should_poll = False
+
+    def __init__(self, hass, entry_id: str, device_entry, key: str, name: str, icon: str, service: str):
         self._hass = hass
         self._device_entry = device_entry
-        self._attr_name = "Start Benchmark"
-        self._attr_unique_id = "benchmark_start"
-        self._attr_should_poll = False
+        self._service = service
+        self._attr_name = name
+        self._attr_icon = icon
+        self._attr_unique_id = f"{entry_id}_{key}_button"
 
     @property
     def device_info(self):
@@ -23,4 +38,4 @@ class BenchmarkStartButton(ButtonEntity):
         }
 
     async def async_press(self):
-        await self._hass.services.async_call(DOMAIN, "start", {})
+        await self._hass.services.async_call(DOMAIN, self._service, {}, blocking=False)
