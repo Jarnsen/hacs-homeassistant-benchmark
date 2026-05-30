469c17e786a1f1962c040670fbd816416cbfca3f
Jarnsen <jarno.karsch@vodafone.de>
2026-05-30 23:44:19 +0200

    Prevent duplicate benchmark config entries

--- custom_components/benchmark/config_flow.py
+++ custom_components/benchmark/config_flow.py
@@ -1,9 +1,19 @@
+from __future__ import annotations
+
 import voluptuous as vol
 from homeassistant import config_entries
+
 from .const import DOMAIN
 
+
 class BenchmarkConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
     async def async_step_user(self, user_input=None):
+        await self.async_set_unique_id(DOMAIN)
+        self._abort_if_unique_id_configured()
+
         if user_input is None:
             return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
-        return self.async_create_entry(title="Benchmark", data={})
+
+        return self.async_create_entry(title="Home Assistant Benchmark", data={})
