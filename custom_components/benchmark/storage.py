c268f4537ac5f81b92630a85606a77bef3344fe7
Jarnsen <jarno.karsch@vodafone.de>
2026-05-31 16:17:14 +0200

    Add storage helpers for benchmark 2.1

---
+++ custom_components/benchmark/storage.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+
+def read_json(path, default):
+    if not os.path.exists(path):
+        return default
+    try:
+        with open(path, encoding='utf-8') as f:
+            return json.load(f)
+    except Exception:
+        return default
+
+
+def atomic_write_json(path, data):
+    tmp = f'{path}.tmp'
+    with open(tmp, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+    os.replace(tmp, path)
